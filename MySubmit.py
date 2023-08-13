119b6d447bf73dc5370be540b7c90bfa3538526c
zejialu <lzj777@sjtu.edu.cn>
2023-08-13 09:19:34 -0500

    add recovery

--- MySubmit.py
+++ MySubmit.py
@@ -4,7 +4,7 @@ import ROOT as R
 hadd_dir = 'hadd' # grid
 
 hadd_tag = 'total'
-ups_setup = '. `/cvmfs/gm2.opensciencegrid.org/prod/external/ups/v6_0_7/Linux64bit+3.10-2.17/bin/ups setup jobsub_client`'
+ups_setup = '. `/cvmfs/gm2.opensciencegrid.org/prod/external/ups/v6_0_7/Linux64bit+3.10-2.17/bin/ups setup jobsub_client v1_3_5`'
 
 def local_test(data_config):
     cmd = 'gm2 -c {0:} -s {1:} -T local_test.root'.format(data_config['fcl'],data_config['test_file'])
@@ -51,6 +51,14 @@ def check_production(data_config):
         production_paths.append(production_path)
     check_status(production_paths)
 
+def check_recovery(data_config):
+    recovery_paths = []
+    for dataset in data_config['dataset']:
+        recovery_path = '{0:}/{1:}_recovery'.format(data_config['output_dir'], dataset)
+        if (os.path.isdir(recovery_path)):
+            recovery_paths.append(recovery_path)
+    check_status(recovery_paths)
+
 def check_hadd(data_config):
     hadd_paths = []
     for dataset in data_config['dataset']:
@@ -63,6 +71,9 @@ def hadd(data_config,is_local,final_path=None):
     for dataset in data_config['dataset']:
         full_path = get_full_path('{0:}/{1:}_gridRun'.format(data_config['output_dir'],dataset))
         input_dir = '{0:}/{1:}'.format(full_path,'data')
+        recovery_path = get_full_path('{0:}/{1:}_recovery'.format(data_config['output_dir'],dataset))
+        recovery_dir = '{0:}/{1:}'.format(recovery_path,'data')
+
         output_dir = '{0:}/{1:}'.format(full_path,hadd_dir)
         os.system('mkdir -p {0:}'.format(output_dir))
         files = commands.getstatusoutput('ls %s/ | wc -l'%(input_dir))[1]
@@ -72,11 +83,32 @@ def hadd(data_config,is_local,final_path=None):
         else:
             os.system('mkdir -p {0:}'.format(final_path))
             label = re.findall('_(run[0-9]+[a-zA-Z]+)_', dataset)[0]
-            cmd = 'hadd -j 10 -f {0:}/{1:}.root {2:}/*.root'.format(final_path,label,input_dir)
+            if (os.path.isdir(recovery_dir)):
+                cmd = 'hadd -j 10 -f {0:}/{1:}.root {2:}/*.root {3:}/*.root'.format(final_path,label,input_dir,recovery_dir)
+            else:
+                cmd = 'hadd -j 10 -f {0:}/{1:}.root {2:}/*.root'.format(final_path,label,input_dir)
             print (cmd)
             # return 
         print (cmd)
         os.system(cmd)
+
+def recovery(data_config):
+    nfile_per_job = 100
+    for dataset in data_config['dataset']:
+        full_path = get_full_path('{0:}/{1:}_gridRun'.format(data_config['output_dir'],dataset))
+        print ('./sam_recovery.sh {0:}'.format(full_path))
+        recovery_message = commands.getstatusoutput('./sam_recovery.sh {0:}'.format(full_path))[1]
+        print(recovery_message)
+        searchObj = re.search(r'SAM recovery dataset created: \'(.*?)\' with (.*?) missing input files.', recovery_message, re.M)
+        if searchObj:
+            recovery_dataset = searchObj.group(1)
+            nfiles = int(searchObj.group(2))
+
+            recovery_path = '{0:}/{1:}_recovery'.format(data_config['output_dir'],dataset)
+            njob = nfiles / nfile_per_job + 1
+            cmd = '{6:};./gridSetupAndSubmitGM2Data.sh --fhicl {0:} --sam-dataset {1:} --output-dir {2:} {3:} --njobs {4:};sleep {5:}'.format(
+            data_config['fcl'],recovery_dataset,recovery_path,data_config['options'],njob,data_config['separation_time'],ups_setup)
+            os.system(cmd)
         
 
 
@@ -167,7 +199,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d','--dataset')
     parser.add_argument('-c','--config',default='production.json')
-    parser.add_argument('-j','--job',choices=['local_test','grid_test','grid_run','hadd','hadd_local','check_production','check_hadd','link_files'])
+    parser.add_argument('-j','--job',choices=['local_test','grid_test','grid_run','hadd','hadd_local','check_production','check_hadd','link_files','recovery','check_recovery'])
     parser.add_argument('-l','--linkdir',default=None)
 
     args = parser.parse_args()    
@@ -193,5 +225,9 @@ if __name__ == '__main__':
         check_hadd(data_config)
     elif args.job == 'link_files':
         link_files(data_config,args.linkdir)
+    elif args.job == 'recovery':
+        recovery(data_config)
+    elif args.job == 'check_recovery':
+        check_recovery(data_config)
     else:
         raise ValueError('unknown job {0:}'.format(args.job))
